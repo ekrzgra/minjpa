69802cbdb74af42fc37671eadb895b7b19925515
krzysztof.graczyk <allez@wp.pl>
2017-02-25 10:06:30 +0100

    annotation tuning

--- src/main/java/minjpa/entities/DailyStatistics.java
+++ src/main/java/minjpa/entities/DailyStatistics.java
@@ -2,13 +2,18 @@ package minjpa.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "dailyStatistics")
 public class DailyStatistics {
 
 	private long id;
@@ -19,18 +24,23 @@ public class DailyStatistics {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "stats_id", unique = true)
 	public long getId() {
 		return id;
 	}
 
+	@Column(name = "snapshot_time", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	public Date getSnapshotTime() {
 		return snapshotTime;
 	}
 
+	@Column(name = "views", nullable = false)
 	public long getViews() {
 		return views;
 	}
 
+	@Column(name = "comments", nullable = false)
 	public long getComments() {
 		return comments;
 	}
--- src/main/java/minjpa/entities/Video.java
+++ src/main/java/minjpa/entities/Video.java
@@ -3,13 +3,20 @@ package minjpa.entities;
 import java.util.Date;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
+@Table(name = "video")
 public class Video {
 
 	private long id;
@@ -20,23 +27,28 @@ public class Video {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true)
 	public long getId() {
 		return id;
 	}
 
+	@Column(name = "name", nullable = false)
 	public String getName() {
 		return name;
 	}
 
+	@Column(name = "created_on", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	public Date getCreatedOn() {
 		return createdOn;
 	}
 
+	@Transient
 	public Boolean isUserHasAccess() {
 		return userHasAccess;
 	}
 
-	@OneToMany
+	@OneToMany(mappedBy = "releatedVideo", cascade = CascadeType.REMOVE)
 	public Set<DailyStatistics> getDailyStatistics() {
 		return dailyStatistics;
 	}
